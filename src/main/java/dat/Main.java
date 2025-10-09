863e0d1912d62928ab06ff0df5dfed39a379f66b
Johan <179227533+JosefBenjamin@users.noreply.github.com>
2025-10-09 21:38:29 +0200

    new tests again, again

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -13,7 +13,7 @@ import org.slf4j.LoggerFactory;
 public class Main {
 
     public static void main(String[] args) {
-        //TODO: Watchtower Test
+        //TODO: Watchtower Test again!!!!
         ApplicationConfig.startServer(7070);
     }
 }
